fa93787639cc65e616bce3ebddb48296d72f3794
Charles Bergeron <chuck.bergeron@gmail.com>
2019-02-16 18:39:49 -0700

    * fix up layout transitions and add a symbol graphic

--- src/components/hooks/LandingHero.jsx
+++ src/components/hooks/LandingHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import VelcroSymbol from '~/assets/images/velcro--symbol.svg'
 
 export const LandingHero = ({ heroColor }) => {
   return (
@@ -6,18 +7,23 @@ export const LandingHero = ({ heroColor }) => {
       <div className='hero-body'>
         <div className='container'>
           <div className='row'>
-            <div className='col-xs-12 col-md-7'>
+            <div className='col-xs-12 col-sm-8'>
+              <div className='row'>
+                <div className='col-xs-6 col-sm-3'>
+                  <VelcroSymbol className='img-responsive' />
+                </div>
+              </div>
+              <br />
+
               <h1 className='is-size-1 is-uppercase is-family-sans-serif'>
-                Velcro
+                Velcro <span className="is-hidden-touch is-size-5"> For Ethereum</span>
               </h1>
               <h4 className='is-size-4 is-family-serif'>
-                Send and receive <strong>(notifications/texts/emails/slacks, etc.)</strong> when your Ethereum Smart Contract events happen.
+                Send and receive <strong>(notifications/texts/emails/slacks, etc.)</strong> when Ethereum Smart Contract events happen.
               </h4>
             </div>
 
-            <div className='col-xs-12 col-md-5'>
-              graphix here
-            </div>
+            
           </div>
         </div>
       </div>
