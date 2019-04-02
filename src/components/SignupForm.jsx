3599cca56c7b4d00ea7b1fa089582984a8777671
Charles Bergeron <chuck.bergeron@gmail.com>
2019-04-01 17:01:36 -0700

    * have a secondary success state on homepage signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import AntdIcon from '@ant-design/icons-react'
+import { MailOutline } from '@ant-design/icons'
+import { CSSTransition } from 'react-transition-group'
 import { axiosInstance } from '~/../config/axiosInstance'
 
 export const SignupForm =
@@ -59,48 +62,82 @@ export const SignupForm =
     }
 
     render () {
+      const thankYou = (
+        <div className='accordion accordion--signup-thank-you'>
+          <p>
+            <AntdIcon type={MailOutline} className='antd-icon icon--signup' />
+            Thanks for using Notus! Your <strong>'{this.state.email}'</strong> inbox should have a magic link to access your Notus account.
+            <br /><span className='is-size-7 has-text-light'>
+              Can't find the email? Check your spam folder first. We can also re-send the link or reach out to us for support.
+            </span>
+          </p>
+        </div>
+      )
+      
+      const form = (
+        <div className='accordion accordion-enter-done accordion--signup-form'>
+          <form
+            onSubmit={this.handleSignupSubmit}
+            className={this.state.active ? 'is-active' : ''}
+          >
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  placeholder='Enter your email'
+                  autoFocus
+                  type='email'
+                  id='email'
+                  className='input'
+                  onFocus={this.activate}
+                  onBlur={this.deactivate}
+                  onMouseOver={this.activate}
+                  onMouseOut={this.deactivate}
+                  onChange={(e) => { this.setState({ email: e.target.value }) }}
+                  value={this.state.email}
+                />
+              </div>
+              <div className='control'>
+                <button
+                  type='submit'
+                  className='button'
+                  onFocus={this.activate}
+                  onBlur={this.deactivate}
+                  onMouseOver={this.activate}
+                  onMouseOut={this.deactivate}
+                >
+                  Get Started
+                  </button>
+              </div>
+            </div>
+            {/* <div className='control checkbox'>
+                                    <label htmlFor='newsletter'>
+                                      <input id='newsletter' className='checkbox-input' type='checkbox' />Stay
+                                      up-to-date with our newsletter
+                                  </label>
+                                  </div> */}
+
+          </form>
+        </div>
+      )
+
       return (
-        <form
-          onSubmit={this.handleSignupSubmit}
-          className={this.state.active ? 'is-active': ''}
-        >
-          <div className="field has-addons">
-            <div className="control">
-              <input
-                placeholder='Enter your email'
-                autoFocus
-                type='email'
-                id='email'
-                className='input'
-                onFocus={this.activate}
-                onBlur={this.deactivate}
-                onMouseOver={this.activate}
-                onMouseOut={this.deactivate}
-                onChange={(e) => { this.setState({ email: e.target.value }) }}
-                value={this.state.email}
-              />
-            </div>
-            <div className='control'>
-              <button
-                type='submit'
-                className='button'
-                onFocus={this.activate}
-                onBlur={this.deactivate}
-                onMouseOver={this.activate}
-                onMouseOut={this.deactivate}
-              >
-                Get Started
-              </button>
-            </div>
-          </div>
-          {/* <div className='control checkbox'>
-                                <label htmlFor='newsletter'>
-                                  <input id='newsletter' className='checkbox-input' type='checkbox' />Stay
-                                  up-to-date with our newsletter
-                              </label>
-                              </div> */}
-          
-        </form>
+        <div>
+          <CSSTransition
+            timeout={600}
+            classNames='accordion'
+            in={!this.state.success}
+          >
+            {state => form}
+          </CSSTransition>
+
+          <CSSTransition
+            timeout={600}
+            classNames='accordion'
+            in={this.state.success}
+          >
+            {state => thankYou}
+          </CSSTransition>
+        </div>
       )
   }
 }
