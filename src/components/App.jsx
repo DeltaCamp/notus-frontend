ab3fce14bd1ff39004ea2d6017bf1b7973472abb
Charles Bergeron <chuck.bergeron@gmail.com>
2019-04-29 10:16:08 -0700

    * complete refactor and merging edit event with show event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ import { TermsPage } from '~/components/pages/TermsPage'
 import { SupportPage } from '~/components/pages/SupportPage'
 import { EventPage } from '~/components/pages/events/EventPage'
 import { MyEventsPage } from '~/components/pages/events/MyEventsPage'
-import { EditEventPage } from '~/components/pages/events/EditEventPage'
+import { EditEventPageWrapper } from '~/components/pages/events/EditEventPageWrapper'
 import { PasswordResetPage } from '~/components/pages/PasswordResetPage'
 import { SignInPage } from '~/components/pages/SignInPage'
 import { SignUpPage } from '~/components/pages/SignUpPage'
@@ -74,9 +74,9 @@ export const App = withRouter(hot(module)(
                 <Route exact path={routes.TERMS_PAGE} component={TermsPage} />
                 <Route exact path={routes.SUPPORT_PAGE} component={SupportPage} />
 
-                <Route exact path={routes.EDIT_EVENT} component={EditEventPage} />
-                <Route exact path={routes.NEW_EVENT} component={EditEventPage} />
-                <Route exact path={routes.NEW_EVENT_FROM_PARENT} component={EditEventPage} />
+                <Route exact path={routes.EDIT_EVENT} component={EditEventPageWrapper} />
+                <Route exact path={routes.NEW_EVENT} component={EditEventPageWrapper} />
+                <Route exact path={routes.NEW_EVENT_FROM_PARENT} component={EditEventPageWrapper} />
                 <Route exact path={routes.MY_EVENTS} component={MyEventsPage} />
                 <Route exact path={routes.DISCOVER_EVENTS} component={DiscoverEventsPage} />
                 <Route exact path={routes.EVENT} component={EventPage} />
---
+++ src/components/events/EditEventButtons.jsx
@@ -0,0 +1,329 @@
+import React, { Component } from 'react'
+import ReactTimeout from 'react-timeout'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import {
+  AlertTriangle,
+  Cast,
+  CheckCircle,
+  PlusCircle,
+  StopCircle,
+} from 'react-feather'
+import { formatRoute } from 'react-router-named-routes'
+import { Link } from 'react-router-dom'
+import { graphql } from 'react-apollo'
+import { toast } from 'react-toastify'
+import { IsAuthed } from '~/components/IsAuthed'
+import { Modal } from '~/components/Modal'
+import { deleteEventMutation } from '~/mutations/deleteEventMutation'
+import { updateEventMutation } from '~/mutations/updateEventMutation'
+import { currentUserQuery } from '~/queries/currentUserQuery'
+import * as routes from '~/../config/routes'
+
+export const EditEventButtons = 
+  IsAuthed(
+    graphql(currentUserQuery, { name: 'currentUserData' })(
+      graphql(updateEventMutation, {
+        name: 'updateEventMutation'
+      })(
+        graphql(deleteEventMutation, {
+          name: 'deleteEventMutation'
+        })(
+          ReactTimeout(
+            class _EditEventButtons extends Component {
+              state = {
+                newEventTitle: '',
+                isConfirmingDelete: false
+              }
+
+              static propTypes = {
+                match: PropTypes.object.isRequired
+              }
+
+              static contextTypes = {
+                router: PropTypes.object.isRequired
+              }
+
+              handleActivate = (e) => {
+                e.preventDefault()
+
+                const event = this.props.eventData.event
+                
+                this.props.updateEventMutation({
+                  variables: {
+                    event: {
+                      id: event.id,
+                      isActive: !event.isActive
+                    }
+                  },
+                  refetchQueries: [
+                    'eventsQuery',
+                    'eventsQuery',
+                  ],
+                }).then(({ data: { updateEvent } }) => {
+                  toast.dismiss()
+                  toast.success(`Event ${updateEvent.isActive ? 're-activated' : 'deactivated'}`)
+                }).catch(error => {
+                  toast.error('Error while updating event')
+                  console.error(error)
+                })
+              }
+
+              handlePublishClick = (e) => {
+                e.preventDefault()
+
+                const event = this.props.eventData.event
+
+                this.props.updateEventMutation({
+                  variables: {
+                    event: {
+                      id: event.id,
+                      isPublic: !event.isPublic
+                    }
+                  },
+                  refetchQueries: [
+                    'eventsQuery',
+                    'eventsQuery',
+                  ],
+                }).then(({ data: { updateEvent } }) => {
+                  toast.dismiss()
+                  toast.success(`Event ${updateEvent.isPublic ? 'published' : 'made private'}`)
+                }).catch(error => {
+                  toast.error('Error while updating event')
+                  console.error(error)
+                })
+              }
+
+              runUpdateEventMutation(variables, successCallback, errorCallback) {
+                if (!errorCallback) {
+                  errorCallback = error => {
+                    console.error(error)
+                  }
+                }
+
+                this.props.updateEventMutation({
+                  variables,
+                  refetchQueries: [
+                    // only refetch the event we just updated (1 record)
+                    'eventsQuery',
+                    'eventsQuery',
+                  ],
+                }).then(successCallback).catch(errorCallback)
+              }
+
+              handleOpenConfirmDeleteModal = (e) => {
+                e.preventDefault()
+
+                this.setState({ isConfirmingDelete: true })
+              }
+
+              handleCloseConfirmDeleteModal = (e) => {
+                e.preventDefault()
+
+                this.setState({ isConfirmingDelete: false })
+              }
+
+              handleDelete = (e) => {
+                e.preventDefault()
+
+                const eventId = parseInt(this.props.eventData.event.id, 10)
+
+                this.props.deleteEventMutation({
+                  variables: {
+                    eventId
+                  },
+                  refetchQueries: [
+                    'eventsQuery',
+                    'eventsQuery',
+                  ],
+                }).then(() => {
+                  toast.dismiss()
+                  toast.success('Successfully deleted event')
+                  this.props.history.push(routes.MY_EVENTS)
+                }).catch(error => {
+                  console.error(error)
+                })
+              }
+
+              render () {
+                let isEventAuthor,
+                  eventAuthorButtons,
+                  notEventAuthorButtons,
+                  event,
+                  eventPublishedState,
+                  eventActiveState,
+                  activeClasses
+
+                const { eventData, currentUserData } = this.props
+                if (eventData) {
+                  const error = eventData.error || currentUserData.error
+                  const loading = eventData.loading || currentUserData.loading
+  
+                  event = eventData.event || {}
+  
+                  if (loading) {
+                    return null
+                  } else if (error) {
+                    console.error(eventData.error)
+                    return null
+                  } else if (!event) {
+                    return null  
+                  }
+  
+                  isEventAuthor = (currentUserData.currentUser && currentUserData.currentUser.id === parseInt(event.user.id, 10))
+                  activeClasses = {
+                    'is-two-thirds-opacity': !event.isActive,
+                      'is-full-opacity': event.isActive
+                  }
+  
+                  if (!isEventAuthor) {
+                    const createEventFromParentRoute = formatRoute(
+                      routes.NEW_EVENT_FROM_PARENT, { eventId: event.id }
+                    )
+  
+                    notEventAuthorButtons = (
+                      <Link
+                        to={createEventFromParentRoute}
+                        className='button is-small is-primary'
+                      >
+                        <PlusCircle /> &nbsp;Create Event From This One
+                      </Link>
+                    )
+                  } else {
+                    eventAuthorButtons = (
+                      <>
+                        <button
+                          className={classnames(
+                            'button',
+                            'is-small',
+                            'is-pink'
+                          )}
+                          onClick={this.handlePublishClick}
+                        >
+                          
+                          {event.isPublic
+                            ? (<><Cast /> &nbsp;Make Private</>)
+                            : (<><Cast /> &nbsp;Make Public</>)
+                          }
+                        </button>
+                        <button
+                          className={classnames(
+                            'button',
+                            'is-small',
+                            {
+                              'is-info': event.isActive,
+                              'is-success': !event.isActive
+                            }
+                          )}
+                          onClick={this.handleActivate}
+                        >
+                          {event.isActive
+                            ? (<><StopCircle /> &nbsp;Deactivate</>)
+                            : (<><CheckCircle /> &nbsp;Activate</>)
+                          }
+                        </button>
+                        <button
+                          className={classnames(
+                            'button',
+                            'is-small',
+                            'is-danger',
+                            {
+                              'is-two-thirds-opacity': !event.isActive,
+                              'is-full-opacity': event.isActive
+                            }
+                          )}
+                          onClick={this.handleOpenConfirmDeleteModal}
+                        >
+                          <AlertTriangle /> &nbsp;Delete
+                      </button>
+                    </>
+                    )
+                  }
+
+                  eventPublishedState = event.isPublic
+                    ? (
+                      <>
+                        <strong>Published:</strong> This will show up as a useable event for other Notus customers sitewide.
+                        </>
+                    )
+                    : (
+                      <>
+                        <strong>Private:</strong> Only you will be able to see this event.
+                        </>
+                    )
+
+                  eventActiveState = event.isActive
+                    ? (
+                      <>
+                        <strong>Active:</strong> currently triggering actions when transactions/blocks/contract events occurs.
+                      </>
+                    )
+                    : (
+                      <>
+                        <strong>Not Active:</strong> Currently not firing when events occur.
+                      </>
+                    )
+                }
+                  
+                return (
+                  <>
+                    <Modal
+                      isOpen={this.state.isConfirmingDelete}
+                      handleClose={this.handleCloseConfirmDeleteModal}
+                      isSmall={true}
+                    >
+                      <div className='has-text-centered'>
+                        <h5 className='is-size-5 has-text-weight-normal'>
+                          Are you sure you want to delete this event?
+                        </h5>
+                        <br/>
+                        <div className='buttons'>
+                          <button
+                            className='button is-small is-light'
+                            onClick={this.handleCloseConfirmDeleteModal}
+                          >
+                            No
+                          </button>
+                          <button
+                            className='button is-small is-success'
+                            onClick={this.handleDelete}
+                          >
+                            Yes
+                          </button>
+                        </div>
+                      </div>
+                    </Modal>
+
+                    <div className={classnames(
+                      'buttons',
+                      activeClasses
+                    )}>
+                      {notEventAuthorButtons}
+                      
+                      {eventAuthorButtons}
+
+                      {this.props.isCreateMode() &&
+                        <button
+                          onClick={this.handleSaveEvent}
+                          className='button is-success'
+                          disabled={this.props.isSubmitting}
+                        >
+                          <PlusCircle />&nbsp;Create Event
+                        </button>
+                      }
+                    </div>
+
+                    <div className='is-size-6'>
+                      {eventPublishedState}
+                      <br />
+                      {eventActiveState}
+                    </div>
+                  </>
+                )
+              }
+            }
+          )
+        )
+      )
+    )
+  )
