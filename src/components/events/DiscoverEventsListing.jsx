9a542c00a3adda3b5dba07f009928231f844329b
Brendan Asselstine <mail.asselstine@gmail.com>
2019-04-29 13:57:23 -0700

    Added paging to DiscoverEventsListing

--- src/components/events/DiscoverEventsListing.jsx
+++ src/components/events/DiscoverEventsListing.jsx
@@ -13,13 +13,41 @@ export const DiscoverEventsListing =
       variables: {
         eventsQuery: {
           isPublic: true,
+          skip: 0,
           take: 1
         }
       }
     }
   })(
     class _DiscoverEventsListing extends PureComponent {
+      fetchMore = () => {
+        const { eventsData } = this.props
+        const { fetchMore, events } = eventsData || {}
+        const { skip, take } = events || {}
+        if (fetchMore) {
+          fetchMore({
+            variables: {
+              eventsQuery: {
+                take,
+                skip: (skip + take),
+                isPublic: true
+              }
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+              if (!fetchMoreResult) return prev;
+              return Object.assign({}, prev, {
+                events: {
+                  ...fetchMoreResult.events,
+                  events: [...prev.events.events, ...fetchMoreResult.events.events]
+                }
+              });
+            }
+          })
+        }
+      }
+
       render () {
+        let loadMore
         const { searchValue } = this.props
 
         if (this.props.eventsData.loading) {
@@ -29,7 +57,7 @@ export const DiscoverEventsListing =
           return null
         }
 
-        const { events } = this.props.eventsData.events
+        const { events, skip, take, totalCount } = this.props.eventsData.events
         let filteredEvents = events
 
         if (searchValue && searchValue.length) {
@@ -52,17 +80,26 @@ export const DiscoverEventsListing =
           filteredEvents = filteredEvents.slice(0, 10)
         }
 
-        return (
-          filteredEvents.length > 0 ?
-              <div className='listing-grid'>
-                {filteredEvents}
-              </div>
-            : (
-              <div className='has-text-centered'>
-                <h2 className='has-text-centered is-size-2'>No events found named '{searchValue}'.</h2>
-              </div>
-            )
-        )
+        if (skip + take < totalCount) {
+          loadMore = <p>
+            <button className='button' onClick={this.fetchMore}>Load More</button>
+          </p>
+        }
+
+        return <>
+          {(
+            filteredEvents.length > 0 ?
+                <div className='listing-grid'>
+                  {filteredEvents}
+                </div>
+              : (
+                <div className='has-text-centered'>
+                  <h2 className='has-text-centered is-size-2'>No events found named '{searchValue}'.</h2>
+                </div>
+              )
+          )}
+          {loadMore}
+        </>
       }
     }
   )
