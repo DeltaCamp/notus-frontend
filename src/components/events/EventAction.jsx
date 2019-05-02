b9ca67f991defe89d35dbeac0b79dcbac72e58ec
Brendan Asselstine <mail.asselstine@gmail.com>
2019-05-02 15:36:55 -0700

    Started working on EventAction

--- src/components/events/EventAction.jsx
+++ src/components/events/EventAction.jsx
@@ -1,22 +1,55 @@
 import React, { PureComponent } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 export const EventAction =
   class _EventAction extends PureComponent {
-    // static propTypes = {
-    // }
+    static propTypes = {
+      webhookUrl: PropTypes.string.isRequired,
+      webhookBody: PropTypes.string.isRequired,
+      onChangeWebhookUrl: PropTypes.func.isRequired,
+      onChangeWebhookBody: PropTypes.func.isRequired
+    }
+
+    constructor (props) {
+      super(props)
+      this.state = {
+        showWebhookForm: false
+      }
+    }
+
+    showWebhookForm = () => {
+      this.setState({
+        showWebhookForm: true
+      })
+    }
 
     render () {
+      const showWebhookForm = !!this.props.webhookUrl || this.state.showWebhookForm
+      let form
+      let state = '... then send me an email'
+      if (showWebhookForm) {
+        state = '...then trigger a webhook'
+
+        form =
+          <form>
+              <WebhookUrlInput
+                value={this.props.webhookUrl}
+                onChange={this.props.onChangeWebhookUrl} />
+          </form>
+      }
+
       return (
         <>
           <div
             className='event-box__variable-wrapper'
-            onClick={this.showEventForm}
           >
-            <div className='event-box__variable event-box__action has-text-right'>
-              {/* ... then turn on my Phillips Hue lightbulb */}
-              ... then send me an email.
-            </div>
+            <button
+              className='event-box__variable event-box__action'
+              onClick={this.showWebhookForm}
+            >
+              {state}
+            </button>&nbsp;
+            {form}
           </div>
         </>
       )
