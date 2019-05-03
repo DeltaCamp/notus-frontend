817b42f3d3954702160d5461211f67d36cf3c50e
Charles Bergeron <chuck.bergeron@gmail.com>
2019-05-03 12:02:29 -0700

    * patch up some mobile bugs

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
+import ReactTimeout from 'react-timeout'
 import { Activity, Power, Search, Send } from 'react-feather'
 import { withRouter } from 'react-router'
 import { Link, NavLink } from 'react-router-dom'
@@ -13,177 +14,198 @@ import * as routes from '~/../config/routes'
 
 export const Nav = graphql(signOutMutation, { name: 'signOutMutation' })(
   graphql(currentUserQuery)(
-    withRouter(class _Nav extends Component {
-      constructor (props) {
-        super(props)
-        this.state = {
-          signedOut: false
+    withRouter(
+      ReactTimeout(class _Nav extends Component {
+        constructor (props) {
+          super(props)
+          this.state = {
+            signedOut: false
+          }
         }
-      }
 
-      state = {
-        mobileNavActive: false
-      }
+        state = {
+          mobileNavActive: false,
+          navIsInFront: false
+        }
 
-      handleToggleMobileNav = (e) => {
-        const mobileNavActive = !this.state.mobileNavActive
+        handleToggleMobileNav = (e) => {
+          if (this.state.mobileNavActive) {
+            this.closeMobileNav()
+          } else {
+            this.setState({ 
+              mobileNavActive: true,
+              navIsInFront: true
+            })
+          }
+        }
 
-        this.setState({ mobileNavActive })
-      }
+        closeMobileNav = (e) => {
+          this.setState({ mobileNavActive: false })
 
-      closeMobileNav = (e) => {
-        this.setState({ mobileNavActive: false })
-      }
+          this.props.setTimeout(() => {
+            this.setState({ navIsInFront: false })
+          }, 1000)
+        }
 
-      handleSignOut = (e) => {
-        this.props.signOutMutation().then(() => {
-          this.closeMobileNav()
+        handleSignOut = (e) => {
+          this.props.signOutMutation().then(() => {
+            this.closeMobileNav()
 
-          notusToast.info('Successfully signed out. Thanks for using Notus!')
-          this.props.history.push(routes.SIGNIN)
-        }).catch(error => {
-          console.error(error)
-        })
-      }
+            notusToast.info('Successfully signed out. Thanks for using Notus!')
+            this.props.history.push(routes.SIGNIN)
+          }).catch(error => {
+            console.error(error)
+          })
+        }
 
-      render () {
-        let signUp, myEvents, signOut
+        render () {
+          let signUp, myEvents, signOut
 
-        if (!this.props.data.currentUser) {
-          signUp = <>
-            <NavLink
-              exact
-              to={routes.SIGNIN}
-              className='navbar-item'
-              onClick={this.closeMobileNav}
-              activeClassName='is-active'
-            >
-            Sign In
-            </NavLink>
-            <NavLink
-              exact
-              to={routes.SIGNUP}
-              className='navbar-item bold'
-              onClick={this.closeMobileNav}
-              activeClassName='is-active'
-            >
-              Sign Up
-            </NavLink>
-          </>
-        } else {
-          signOut = (
-            <>
+          if (!this.props.data.currentUser) {
+            signUp = <>
               <NavLink
                 exact
-                className='navbar-item bold hide-not-home button'
-                to={routes.MY_EVENTS}
+                to={routes.SIGNIN}
+                className='navbar-item'
                 onClick={this.closeMobileNav}
                 activeClassName='is-active'
               >
-                <Send />&nbsp;Go To App
+              Sign In
               </NavLink>
-              {/* <NavLink
+              <NavLink
                 exact
-                className='navbar-item hide-on-home'
-                to={routes.ACCOUNT_SETTINGS}
+                to={routes.SIGNUP}
+                className='navbar-item bold'
                 onClick={this.closeMobileNav}
-                activeClassName="is-active"
+                activeClassName='is-active'
               >
-                <Star />&nbsp;Your Account
+                Sign Up
               </NavLink>
-              */}
-              <button
-                className='navbar-item button hide-on-home'
-                onClick={this.handleSignOut}
-              >
-                <Power />&nbsp;Sign Out
-              </button>
+            </>
+          } else {
+            signOut = (
+              <>
+                <NavLink
+                  exact
+                  className='navbar-item bold hide-not-home button'
+                  to={routes.MY_EVENTS}
+                  onClick={this.closeMobileNav}
+                  activeClassName='is-active'
+                >
+                  <Send />&nbsp;Go To App
+                </NavLink>
+                {/* <NavLink
+                  exact
+                  className='navbar-item hide-on-home'
+                  to={routes.ACCOUNT_SETTINGS}
+                  onClick={this.closeMobileNav}
+                  activeClassName="is-active"
+                >
+                  <Star />&nbsp;Your Account
+                </NavLink>
+                */}
+                <button
+                  className='navbar-item button hide-on-home'
+                  onClick={this.handleSignOut}
+                >
+                  <Power />&nbsp;Sign Out
+                </button>
+              </>
+            )
+
+            myEvents =
+              <>
+                <NavLink
+                  exact
+                  to={routes.MY_EVENTS}
+                  className='navbar-item'
+                  onClick={this.closeMobileNav}
+                  activeClassName='is-active'
+                >
+                  <Activity />&nbsp;My Events
+                </NavLink>
+                <NavLink
+                  exact
+                  to={routes.DISCOVER_EVENTS}
+                  className='navbar-item'
+                  onClick={this.closeMobileNav}
+                  activeClassName='is-active'
+                >
+                  <Search />&nbsp;Discover Events
+                </NavLink>
+              </>
+          }
+
+          return (
+            <>
+              <div
+                className={classnames(
+                  'nav-background',
+                  'no-select', 
+                  {
+                    'is-active': this.state.mobileNavActive
+                  }
+                )}
+                onClick={this.closeMobileNav}
+              />
+
+              <nav className={classnames(
+                'navbar',
+                'is-transparent',
+                {
+                  'is-active': this.state.mobileNavActive,
+                  'is-top-layer': this.state.navIsInFront
+                }
+              )}>
+                <div className='container'>
+                  <div className='row navbar-menu-container'>
+                    <div className='navbar-brand col-xs-6 col-md-1 col-lg-1'>
+                      <Link
+                        to={routes.HOME}
+                        className='navbar-item'
+                        onClick={this.closeMobileNav}
+                      >
+                        <NotusLogo />
+                      </Link>
+                    </div>
+
+                    <div className='col-xs-6 is-hidden-tablet has-text-right'>
+                      <button
+                        className={classnames(
+                          'burger',
+                          'burger-slip',
+                          { 'open': this.state.mobileNavActive }
+                        )}
+                        data-target='navbar-menu'
+                        onClick={this.handleToggleMobileNav}
+                      >
+                        <div className='burger-lines' />
+                      </button>
+                    </div>
+
+                    <div id='navbar-menu' className={classnames(
+                      'navbar-menu',
+                      'col-xs-12',
+                      'col-md-10',
+                      'col-lg-11',
+                      { 'is-active': this.state.mobileNavActive }
+                    )}>
+                      <div className='navbar-start'>
+                        {myEvents}
+                      </div>
+                      <div className='navbar-end'>
+                        {signUp}
+                        {signOut}
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </nav>
             </>
           )
-
-          myEvents =
-            <>
-              <NavLink
-                exact
-                to={routes.MY_EVENTS}
-                className='navbar-item'
-                onClick={this.closeMobileNav}
-                activeClassName='is-active'
-              >
-                <Activity />&nbsp;My Events
-              </NavLink>
-              <NavLink
-                exact
-                to={routes.DISCOVER_EVENTS}
-                className='navbar-item'
-                onClick={this.closeMobileNav}
-                activeClassName='is-active'
-              >
-                <Search />&nbsp;Discover Events
-              </NavLink>
-            </>
         }
-
-        return (
-          <>
-            <div
-              className={classnames('nav-background no-select', { 'is-active': this.state.mobileNavActive })}
-              onClick={this.closeMobileNav}
-            />
-
-            <nav className={classnames(
-              'navbar',
-              'is-transparent',
-              { 'is-active': this.state.mobileNavActive }
-            )}>
-              <div className='container'>
-                <div className='row navbar-menu-container'>
-                  <div className='navbar-brand col-xs-6 col-md-1 col-lg-1'>
-                    <Link
-                      to={routes.HOME}
-                      className='navbar-item'
-                      onClick={this.closeMobileNav}
-                    >
-                      <NotusLogo />
-                    </Link>
-                  </div>
-
-                  <div className='col-xs-6 is-hidden-tablet has-text-right'>
-                    <button
-                      className={classnames(
-                        'burger',
-                        'burger-slip',
-                        { 'open': this.state.mobileNavActive }
-                      )}
-                      data-target='navbar-menu'
-                      onClick={this.handleToggleMobileNav}
-                    >
-                      <div className='burger-lines' />
-                    </button>
-                  </div>
-
-                  <div id='navbar-menu' className={classnames(
-                    'navbar-menu',
-                    'col-xs-12',
-                    'col-md-10',
-                    'col-lg-11',
-                    { 'is-active': this.state.mobileNavActive }
-                  )}>
-                    <div className='navbar-start'>
-                      {myEvents}
-                    </div>
-                    <div className='navbar-end'>
-                      {signUp}
-                      {signOut}
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </nav>
-          </>
-        )
-      }
-    })
+      })
+    )
   )
 )
