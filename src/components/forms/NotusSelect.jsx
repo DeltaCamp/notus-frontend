c4d19f5e79741b1b571797010f0e28848be2726d
Charles Bergeron <chuck.bergeron@gmail.com>
2019-05-31 12:02:38 -0700

    * further refinements to react select UI

--- src/components/forms/NotusSelect.jsx
+++ src/components/forms/NotusSelect.jsx
@@ -15,7 +15,7 @@ const Menu = props => {
         backgroundColor: 'white',
         borderRadius: 4,
         boxShadow: `0 0 0 1px ${shadow}, 0 4px 11px ${shadow}`,
-        marginTop: 8,
+        marginTop: 1,
         position: 'absolute',
         zIndex: 2,
       }}
@@ -65,7 +65,7 @@ const DropdownIndicator = () => (
   </div>
 );
 const ChevronDown = () => (
-  <Svg style={{ marginRight: -6 }}>
+  <Svg style={{ marginRight: -16 }}>
     <path
       d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z"
       fill="currentColor"
